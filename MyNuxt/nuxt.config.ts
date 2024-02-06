d6dc8ad569dab5e7418174413611a3e681b5b513
bulat <farukshin@molot.ru>
2024-02-06 13:47:36 +0300

    * init проекта

---
+++ MyNuxt/nuxt.config.ts
@@ -0,0 +1,15 @@
+import { resolve } from "path";
+
+export default defineNuxtConfig({
+  alias: {
+    "@": resolve(__dirname, "/"),
+  },
+  devtools: { enabled: true },
+  css: ["~/assets/css/main.scss"],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+});
