90faf35eae6f5e460cd6ab8edec976faf3603919
bulat <farukshin@molot.ru>
2024-02-06 18:53:18 +0300

    * middleware

---
+++ MyNuxt/middleware/auth.ts
@@ -0,0 +1,10 @@
+export default defineNuxtRouteMiddleware((to, from) => {
+  /*const isLoggedIn = false;
+  if (isLoggedIn) {
+    return navigateTo(to.fullPath);
+  }
+
+  return navigateTo("/login");*/
+
+  console.log(to, from);
+});
--- MyNuxt/pages/index.vue
+++ MyNuxt/pages/index.vue
@@ -2,6 +2,10 @@
   <div class="Main">Main page</div>
 </template>
 <script>
+definePageMeta({
+  middleware: "auth",
+});
+
 const { sayHello } = useUtils();
 sayHello();
 </script>
