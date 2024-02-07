2e888a849dabef9c9a8423341e853db3f03993fd
bulat <farukshin@molot.ru>
2024-02-07 14:37:55 +0300

    *пример  работы с Pinia

--- MyNuxt/components/counter.vue
+++ MyNuxt/components/counter.vue
@@ -1,11 +1,20 @@
-<script setup lang="ts">
-const counter = useCounter();
+<script setup>
+import { useCounterStore } from "~/stores/myStore";
+
+// access the `store` variable anywhere in the component ✨
+//const { increment, doubleCount, count } = useCounterStore();
+
+const counter = useCounterStore();
+
+//console.log(increment);
 </script>
 
 <template>
   <div>
-    Counter: {{ counter }}
-    <button @click="counter++">+</button>
-    <button @click="counter--">-</button>
+    Counter: {{ counter.count }} Counter: {{ counter.doubleCount }}
+    <button @click="counter.increment">+</button>
+    <button @click="counter.increment">-</button>
+
+    <!--Counter id="counter"></Counter-->
   </div>
 </template>
--- MyNuxt/pages/index.vue
+++ MyNuxt/pages/index.vue
@@ -1,12 +1,19 @@
-<script setup lang="ts">
-const counter = useCounter();
+<script setup>
+import { useCounterStore } from "~/stores/myStore";
+
+// access the `store` variable anywhere in the component ✨
+//const { increment, doubleCount, count } = useCounterStore();
+
+const counter = useCounterStore();
+
+//console.log(increment);
 </script>
 
 <template>
   <div>
-    Counter: {{ counter }}
-    <button @click="counter++">+</button>
-    <button @click="counter--">-</button>
+    Counter: {{ counter.count }} Counter: {{ counter.doubleCount }}
+    <button @click="counter.increment">+</button>
+    <button @click="counter.increment">-</button>
 
     <Counter id="counter"></Counter>
   </div>
